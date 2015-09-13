5c625609876dcac1f9ff4cca27eb78731e16ec12
Mikael Brunila <plattandy@Mikaels-MBP-3.zyxel.com>
2015-09-13 23:40:34 +0300

    sql päivitetty

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO Tweets (id, user_id, tweet_text, name) VALUES ('4567', '123', 'testing', 'Mikael Brunila'); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
-INSERT INTO Users (id, name, screen_name) VALUES ('123', 'mikaelbrunila', 'Mikael Brunila');
-INSERT INTO Hashtags (ht_text, indices) VALUES ('890', '1');
+INSERT INTO Tweets (user_id, tweet_text, name) VALUES ('123', 'testing', 'Mikael Brunila'); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
+INSERT INTO Users (name, screen_name) VALUES ('mikaelbrunila', 'Mikael Brunila');
+INSERT INTO Hashtags (ht_text, indices) VALUES ('#vaalit2015', '1');
 
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,12 +1,21 @@
 CREATE TABLE Users(
    id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
+   user_id bigint NOT NULL,
    name varchar(140) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
    screen_name varchar(140) NOT NULL
  );
  
+   CREATE TABLE Hashtags(
+   id SERIAL PRIMARY KEY,
+   ht_text varchar(140) NOT NULL, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
+   indices integer NOT NULL -- Muista erottaa sarakkeiden määrittelyt pilkulla!
+);
+
  CREATE TABLE Tweets(
    id SERIAL PRIMARY KEY,
-   user_id bigint REFERENCES Users(id),
+   user_id BIGINT REFERENCES Users(id),
+   hashtag_id INTEGER REFERENCES Hashtags(id),
+   tweet_id, bigint NOT NULL,
    tweet_text varchar(140) NOT NULL,
    name varchar(50) NOT NULL,
    coordinates text,
@@ -16,11 +25,7 @@ CREATE TABLE Users(
    place_id text,
    place_type text,
    created_at text,
-   hashtags bigint REFERENCES Hashtags(ht_text)
+   hashtags varchar(140)
  );
  
-   CREATE TABLE Hashtags(
-   ht_text SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
-   indices integer[] NOT NULL -- Muista erottaa sarakkeiden määrittelyt pilkulla!
-);
 
