d3a66de5433ac9b4eaad93e5db2beff92a3faf7b
Mikael Brunila <plattandy@Mikaels-MBP-3.zyxel.com>
2015-09-13 23:47:03 +0300

    sql päivitetty, pieniä muutoksia suhteessa aiempaan dokumentaatioon

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO Tweets (user_id, tweet_text, name) VALUES ('123', 'testing', 'Mikael Brunila'); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
+INSERT INTO Tweets (user_id, tweet_id, tweet_text, name) VALUES ('123', ,'4567', 'testing', 'Mikael Brunila'); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
 INSERT INTO Users (name, screen_name) VALUES ('mikaelbrunila', 'Mikael Brunila');
 INSERT INTO Hashtags (ht_text, indices) VALUES ('#vaalit2015', '1');
 
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -3,19 +3,19 @@ CREATE TABLE Users(
    user_id bigint NOT NULL,
    name varchar(140) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
    screen_name varchar(140) NOT NULL
- );
+);
  
-   CREATE TABLE Hashtags(
+CREATE TABLE Hashtags(
    id SERIAL PRIMARY KEY,
-   ht_text varchar(140) NOT NULL, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
-   indices integer NOT NULL -- Muista erottaa sarakkeiden määrittelyt pilkulla!
+   ht_text varchar(140) NOT NULL,
+   indices integer NOT NULL 
 );
 
- CREATE TABLE Tweets(
+CREATE TABLE Tweets(
    id SERIAL PRIMARY KEY,
    user_id BIGINT REFERENCES Users(id),
    hashtag_id INTEGER REFERENCES Hashtags(id),
-   tweet_id, bigint NOT NULL,
+   tweet_id bigint NOT NULL,
    tweet_text varchar(140) NOT NULL,
    name varchar(50) NOT NULL,
    coordinates text,
@@ -26,6 +26,6 @@ CREATE TABLE Users(
    place_type text,
    created_at text,
    hashtags varchar(140)
- );
+);
  
 
